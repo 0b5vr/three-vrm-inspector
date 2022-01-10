5c338a44c39b52d7fb7ac9c4344e11107574f068
0b5vr <0b5vr@0b5vr.com>
2022-01-11 02:56:11 +0900

    hotfix: oh shit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig( {
   plugins: [ react() ],
-  assetsInclude: [ '**/*.{vrm,fbx}' ]
+  assetsInclude: [ '**/*.{vrm,fbx}' ],
+  base: './',
 } );
