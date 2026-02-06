e6823133aaab680065a0960c6939d8a74fe1a85e
0b5vr <0b5vr@0b5vr.com>
2026-02-06 17:38:50 +0900

    deps: Bump tailwind to latest
    Ref: https://github.com/0b5vr/0b5vr.github.io/commit/54b6fe6a871c33d0f1dcd14a8dbd162c07038ba5

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
-export default defineConfig( {
-  plugins: [ react() ],
-  assetsInclude: [ '**/*.{vrm,fbx}' ],
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  assetsInclude: ['**/*.{vrm,fbx}'],
   base: './',
-} );
+});
