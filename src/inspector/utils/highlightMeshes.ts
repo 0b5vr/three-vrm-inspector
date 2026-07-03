c59b668e5a81c0e34079a0531c6be977428ad37f
0b5vr <0b5vr@0b5vr.com>
2026-07-03 18:54:49 +0900

    fix: highlightMeshes, fix when the mesh doesn't have multiple materials

--- src/inspector/utils/highlightMeshes.ts
+++ src/inspector/utils/highlightMeshes.ts
@@ -1,4 +1,15 @@
+import { MToonMaterial } from '@pixiv/three-vrm';
 import * as THREE from 'three';
+
+function isMToonOutline(material: THREE.Material): boolean {
+  if (!('isMToonMaterial' in material)) { return false; }
+  const mToon = material as MToonMaterial;
+  return mToon.isOutline;
+}
+
+const invisibleMaterial = new THREE.MeshBasicMaterial({
+  visible: false,
+});
 
 const highlightMaterial = new THREE.MeshNormalMaterial({
   transparent: true,
@@ -15,18 +26,31 @@ export function highlightMeshes(
   meshes: THREE.Mesh[],
   material: THREE.Material = highlightMaterial,
 ): () => void {
-  const meshMaterialMap: Map<THREE.Mesh, THREE.Material> = new Map();
+  const meshMaterialMap: Map<THREE.Mesh, THREE.Material | THREE.Material[]> = new Map();
 
-  meshes.forEach((mesh) => {
+  for (const mesh of meshes) {
     if (Array.isArray(mesh.material)) {
-      meshMaterialMap.set(mesh, mesh.material[0]);
-      mesh.material[0] = material;
+      meshMaterialMap.set(mesh, mesh.material.concat());
+      for (let i = 0; i < mesh.material.length; i++) {
+        if (isMToonOutline(mesh.material[i])) {
+          mesh.material[i] = invisibleMaterial;
+        } else {
+          mesh.material[i] = material;
+        }
+      }
+    } else {
+      meshMaterialMap.set(mesh, mesh.material);
+      if (isMToonOutline(mesh.material)) {
+        (mesh.material as THREE.Material) = invisibleMaterial;
+      } else {
+        (mesh.material as THREE.Material) = material;
+      }
     }
-  });
+  }
 
   return (): void => {
-    for (const [mesh, mtl] of meshMaterialMap) {
-      (mesh.material as THREE.Material[])[0] = mtl;
+    for (const [mesh, materialOrMaterials] of meshMaterialMap) {
+      mesh.material = materialOrMaterials;
     }
   };
 }
