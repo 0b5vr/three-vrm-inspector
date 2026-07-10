9ba2c3d0b30dce4ebdc132f36c8a604ec3b5f2c5
0b5vr <0b5vr@0b5vr.com>
2026-07-10 16:30:15 +0900

    feat: improve springbone highlighter
    make helperRoot visible when highlighting even when it's disabled in helpers pane

--- src/inspector/highlighter-functions/highlightVRM1SpringBoneSpring.ts
+++ src/inspector/highlighter-functions/highlightVRM1SpringBoneSpring.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import * as V1SpringBoneSchema from '@pixiv/types-vrmc-springbone-1.0';
 import { Colors } from '../../constants/Colors';
 import { HighlighterRuleFunction } from '../Highlighter';
-import { VRMSpringBoneJoint, VRMSpringBoneJointHelper } from '@pixiv/three-vrm';
+import { VRMSpringBoneJointHelper } from '@pixiv/three-vrm';
 
 const colorConstant = new THREE.Color(Colors.constant);
 
@@ -12,38 +12,27 @@ export const highlightVRM1SpringBoneSpring: HighlighterRuleFunction = async (
 ) => {
   const indexNum = parseInt(index, 10);
 
-  const springBoneManager = inspector.model!.vrm!.springBoneManager!;
-  const nodeJointMap = new Map<THREE.Object3D, VRMSpringBoneJoint>();
-  for (const joint of springBoneManager.joints) {
-    nodeJointMap.set(joint.bone, joint);
+  const springBoneDef = json.extensions!.VRMC_springBone as V1SpringBoneSchema.VRMCSpringBone;
+  const springDef = springBoneDef.springs?.[indexNum];
+  const nodeIndices = springDef?.joints?.map((joint) => joint.node);
+  if (nodeIndices == null || nodeIndices.length < 2) {
+    return () => {};
   }
 
   const helperRoot = inspector.helpersPlugin.springBoneJointHelperRoot;
-  const jointHelperMap = new Map<VRMSpringBoneJoint, VRMSpringBoneJointHelper>();
-  helperRoot.children.forEach((child) => {
-    const helper = child as VRMSpringBoneJointHelper;
-    jointHelperMap.set(helper.springBone, helper);
-  });
-
   const callbacks: (() => void)[] = [];
 
-  const springBoneDef = json.extensions!.VRMC_springBone as V1SpringBoneSchema.VRMCSpringBone;
-  const springDef = springBoneDef.springs![indexNum];
-  const nodeIndices = springDef.joints.map((joint) => joint.node);
+  const nodes = await Promise.all(
+    nodeIndices.map((nodeIndex) => parser.getDependency('node', nodeIndex) as Promise<THREE.Object3D>),
+  );
 
-  const helpers = new Set<VRMSpringBoneJointHelper>();
-  await Promise.all(nodeIndices.map(async (nodeIndex) => {
-    const node = await parser.getDependency('node', nodeIndex) as THREE.Object3D;
-    const joint = nodeJointMap.get(node);
-    if (joint == null) { return; }
-
-    const helper = jointHelperMap.get(joint);
-    if (helper == null) {
-      // since the last joint does not have actual joint this happens very often
-      return;
-    }
-
-    helpers.add(helper);
+  nodes.slice(0, -1).forEach((node, i) => {
+    const childNode = nodes[i + 1];
+    const helper = helperRoot.children.find((helperCandidate) => {
+      const springBone = (helperCandidate as VRMSpringBoneJointHelper).springBone;
+      return springBone.bone === node && springBone.child === childNode;
+    }) as VRMSpringBoneJointHelper | undefined;
+    if (helper == null) { return; }
 
     // TODO: setColor
     const line = helper.children[0] as THREE.LineSegments;
@@ -54,7 +43,16 @@ export const highlightVRM1SpringBoneSpring: HighlighterRuleFunction = async (
     callbacks.push(() => {
       material.color.copy(prevColor);
     });
-  }));
+  });
+
+  if (callbacks.length > 0) {
+    const prevVisible = helperRoot.visible;
+    helperRoot.visible = true;
+
+    callbacks.push(() => {
+      helperRoot.visible = prevVisible;
+    });
+  }
 
   return () => {
     callbacks.forEach((callback) => callback());
